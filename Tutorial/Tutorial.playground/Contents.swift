1002655d16c2dcc0877c891be6312349b8bea580
Min-ki Choe <44465330+minkichoe@users.noreply.github.com>
2019-11-12 14:38:24 +0900

    feat: 인덱스된 이름 고계함수 사용하여 출력
    LOL 챔피언의 이름을 주어진 인덱스에 맞추어 출력하기
    
    고계함수 사용 목록:
      .compactMap()
      .first()
    
    * tokijh님 PR 참고하였습니다.

--- Tutorial/Tutorial.playground/Contents.swift
+++ Tutorial/Tutorial.playground/Contents.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-/*
+/* champion json formed with:
  [
     {
         "key": "266",
@@ -9,24 +9,32 @@ import Foundation
     },
     ...
  ]
- */
-let champsFilePath = Bundle.main.path(forResource: "champs", ofType: "json")
+*/
+/* champion-index json formed with:
+[
+   1,
+   33,
+   ...
+]
+*/
 
-/*
- [
-    1,
-    33,
-    ...
- ]
- */
+struct Champion: Codable {
+    let key: String
+    let name: String
+}
+
+let champsFilePath = Bundle.main.path(forResource: "champs", ofType: "json")
 let selectedIndexesFilePath = Bundle.main.path(forResource: "selectedIndexes", ofType: "json")
 
 let champsData = FileManager.default.contents(atPath: champsFilePath!)
 let selectedIndexesData = FileManager.default.contents(atPath: selectedIndexesFilePath!)
 
-let champs = try JSONSerialization.jsonObject(with: champsData!, options: [])
-let selectedIndexes = try JSONSerialization.jsonObject(with: selectedIndexesData!, options: [])
+let champs = try JSONDecoder().decode([Champion].self, from: champsData!)
+let selectedIndexes = try JSONDecoder().decode([Int].self, from: selectedIndexesData!)
 
 // TODO: selectedIndexes는 챔피언 목록(champs)의 key 번호 들이다. selectedIndexes에 명시된 순서대로 챔피언들의 이름(name)을 나열하라
-let names: [String] = []
+let names: [String] = selectedIndexes.compactMap { index in
+    champs.first { $0.key == "\(index)" }?.name
+}
 print(names)
+
